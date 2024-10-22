1ed10178818a8dc6cb0b638328a6802fc10560c7
ShauryaRoy <shauryaroy2004@gmail.com>
2024-10-22 10:46:06 +0530

    Changed to expo --Login Interface

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        convergence: ['convergence', 'san-serif']
+      }
+
+    },
+  },
+  plugins: [],
+}
+
